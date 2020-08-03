63330b6d6ac53b68ae34aa0904171f3b3b9554b0
its-me-ilia <its.me.ilia04@gmail.com>
2020-08-03 19:07:48 +0400

    browser router to hash router

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React from "react";
 import "./App.css";
 import Intro from "./components/Intro/Intro";
 import NotFound from "./components/404/404";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { Switch, Route, HashRouter } from "react-router-dom";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route exact path="/" render={() => Intro()} />
         <Route exact path="/404" render={() => NotFound()} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
